e11440bab03a8f356b8342a558150bcfb0da67ce
Sven Olding <solding@gedys-intraware.de>
2023-09-17 19:09:33 +0200

    remove CORS config

--- src/main/java/microservices/book/gamification/game/LeaderBoardController.java
+++ src/main/java/microservices/book/gamification/game/LeaderBoardController.java
@@ -2,7 +2,6 @@ package microservices.book.gamification.game;
 
 import lombok.RequiredArgsConstructor;
 import microservices.book.gamification.game.domain.LeaderBoardRow;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,6 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/leaders")
-@CrossOrigin
 public class LeaderBoardController {
     private final LeaderBoardService leaderBoardService;
 
