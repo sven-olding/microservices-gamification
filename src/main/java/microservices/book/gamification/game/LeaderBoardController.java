ca7eb04d58caaccd2962acb461467bd3da2df32d
Sven Olding <solding@gedys-intraware.de>
2023-09-30 17:04:37 +0200

    Update pom.xml

--- src/main/java/microservices/book/gamification/game/LeaderBoardController.java
+++ src/main/java/microservices/book/gamification/game/LeaderBoardController.java
@@ -1,6 +1,7 @@
 package microservices.book.gamification.game;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import microservices.book.gamification.game.domain.LeaderBoardRow;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/leaders")
 public class LeaderBoardController {
@@ -16,6 +18,7 @@ public class LeaderBoardController {
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
         return leaderBoardService.getCurrentLeaderBoard();
     }
 }
